32a885406731faac303272493b146c8019474c55
joserobjr <joserobjr@powernukkit.org>
2022-04-18 18:40:27 -0300

    Add guest parameter to API calls

--- update-on-reinstall/src/install-script.sh
+++ update-on-reinstall/src/install-script.sh
@@ -9,11 +9,11 @@ if [ -n "${DL_PATH}" ]; then
     echo -e "Using supplied download url: ${DL_PATH}"
     DOWNLOAD_URL=`eval echo $(echo ${DL_PATH} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 else
-    LAST_SUCCESS=`curl -sLH 'accept: application/json' -X 'GET' "https://builds.powernukkit.org/app/rest/builds/?locator=buildType:${RELEASE_CHANNEL},status:success,count:1"`
+    LAST_SUCCESS=`curl -sLH 'accept: application/json' -X 'GET' "https://builds.powernukkit.org/app/rest/builds/?locator=buildType:${RELEASE_CHANNEL},status:success,count:1&guest=1"`
     EXISTS=`echo "$LAST_SUCCESS" | jq '.count == 1' 2> /dev/null`
     if [ "${EXISTS}" != "true" ]; then
         RELEASE_CHANNEL=PowerNukkit_Releases
-        LAST_SUCCESS=`curl -sLH 'accept: application/json' -X 'GET' "https://builds.powernukkit.org/app/rest/builds/?locator=buildType:${RELEASE_CHANNEL},status:success,count:1"`
+        LAST_SUCCESS=`curl -sLH 'accept: application/json' -X 'GET' "https://builds.powernukkit.org/app/rest/builds/?locator=buildType:${RELEASE_CHANNEL},status:success,count:1&guest=1"`
         EXISTS=`echo "$LAST_SUCCESS" | jq '.count == 1' 2> /dev/null`
         if [ "${EXISTS}" != "true" ]; then
             echo "Could not update the server jar!"
@@ -21,12 +21,12 @@ else
     fi
     if [ "${EXISTS}" == "true" ]; then
         BUILD_NUMBER=`echo "$LAST_SUCCESS" | jq '.build[0].id'`
-        FILE=`curl -sLH 'accept: application/json' -X 'GET' "https://builds.powernukkit.org/app/rest/builds/${BUILD_NUMBER}/artifacts"`
+        FILE=`curl -sLH 'accept: application/json' -X 'GET' "https://builds.powernukkit.org/app/rest/builds/${BUILD_NUMBER}/artifacts?guest=1"`
         FILE=`echo $FILE | jq '.file[] | select(.name | endswith("-shaded.jar")) | .content.href' 2> /dev/null`
         if [ -z "$FILE" ]; then
             echo "Could not find the artifact of the build $BUILD_NUMBER to update the server jar!"
         else
-            DOWNLOAD_URL=`echo "https://builds.powernukkit.org$FILE" | sed 's/"//g'`
+            DOWNLOAD_URL=`echo "https://builds.powernukkit.org$FILE?guest=1" | sed 's/"//g'`
 			echo "Found a download at $DOWNLOAD_URL"
         fi
     fi
